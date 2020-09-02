5140e97b14650b11d32489110ff08a6c0d1e0909
d4vves <d4vves@gmail.com>
2020-09-02 12:03:45 -0500

    delete project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export default function Project() {
     let { id } = useParams()
     const [currentProject, setCurrentProject] = useState('')
+    const [projectDeleted, setProjectDeleted] = useState(false)
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API}/projects/${id}`)
@@ -16,9 +18,23 @@ export default function Project() {
         .catch(err => console.log(err))
     }, [id])
 
+    const deleteProject = () => {
+        axios.delete(`${process.env.REACT_APP_API}/projects/${id}`)
+        .then(response => {
+            if (response.status === 200) {
+                setProjectDeleted(true)
+            }
+        })
+    }
+
+    if (projectDeleted) {
+        return <Redirect to={`/profile`} />
+    }
+
     return (
         <div>
             <h1>{currentProject.title}</h1>
+            <button onClick={deleteProject}>Delete</button>
             <p>{currentProject.description}</p>
         </div>
     )
